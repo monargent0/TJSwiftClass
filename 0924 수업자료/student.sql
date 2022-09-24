75b1f86b96f685b6bd55665cea948d5e9ed087dc
monargent0 <100196951+monargent0@users.noreply.github.com>
2022-09-24 16:23:02 +0900

    수업자료보충

---
+++ 0924 수업자료/student.sql
@@ -0,0 +1,11 @@
+use education;
+SELECT * FROM student;
+# 데이터 삽입 
+insert into student 
+(scode, sname, sdept, sphone) 
+values
+('s001','박소명','컴퓨터공학과','123-3456');
+insert into student 
+(scode, sname, sdept, sphone) 
+values
+('s002','최민국','컴퓨터공학과','124-3456');
